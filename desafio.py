2f62e259f53385161f2588fc2b505d0da614ac8d
marceloreis951 <marcelo951reis@gmail.com>
2026-08-19 21:20:08 -0300

    Add files via upload
    Conversor de Dollar para Real

---
+++ desafio.py
@@ -0,0 +1,21 @@
+#desafio: Criar um sistema que faça a conversão de reais em dollar
+
+Dollar = 5.18
+
+Real = float(input("Digite o Valor"))
+
+conversao = Real/Dollar
+
+Resultado = conversao
+
+print(f"Voce tera em Real:{Resultado}")
+
+
+
+
+
+
+
+
+
+
